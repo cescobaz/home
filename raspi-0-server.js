c72b783bb87c70a508d55bfd53376098e691092e
Francesco Burelli <francesco.burelli@gmail.com>
2020-11-21 16:13:09 +0100

    typo

--- raspi-0-server.js
+++ raspi-0-server.js
@@ -14,11 +14,11 @@ function runServer () {
   const kitchenLamp = makeLedThing({ pin: 24, identifier: 'kitchen-lamp-0', name: 'kitchen lamp', isLight: true })
   const kitchenBuzzer = makeLedThing({ pin: 8, identifier: 'kitchen-buzzer-0', name: 'kitchen buzzer' })
   const kitchenLed1 = makeLedThing({ pin: 11, identifier: 'kitchen-led-green-1', name: 'kitchen led green 1', isLight: true })
-  const kitchenLed2 = makeLedThing({ ping: 9, identifier: 'kitchen-led-green-2', name: 'kitchen led green 2', isLight: true })
-  const kitchenLed3 = makeLedThing({ ping: 10, identifier: 'kitchen-led-yellow-3', name: 'kitchen led yellow 3', isLight: true })
-  const kitchenLed4 = makeLedThing({ ping: 22, identifier: 'kitchen-led-yellow-4', name: 'kitchen led yellow 4', isLight: true })
-  const kitchenLed5 = makeLedThing({ ping: 17, identifier: 'kitchen-led-red-5', name: 'kitchen led red 5', isLight: true })
-  const kitchenLed6 = makeLedThing({ ping: 4, identifier: 'kitchen-led-red-6', name: 'kitchen led red 6', isLight: true })
+  const kitchenLed2 = makeLedThing({ pin: 9, identifier: 'kitchen-led-green-2', name: 'kitchen led green 2', isLight: true })
+  const kitchenLed3 = makeLedThing({ pin: 10, identifier: 'kitchen-led-yellow-3', name: 'kitchen led yellow 3', isLight: true })
+  const kitchenLed4 = makeLedThing({ pin: 22, identifier: 'kitchen-led-yellow-4', name: 'kitchen led yellow 4', isLight: true })
+  const kitchenLed5 = makeLedThing({ pin: 17, identifier: 'kitchen-led-red-5', name: 'kitchen led red 5', isLight: true })
+  const kitchenLed6 = makeLedThing({ pin: 4, identifier: 'kitchen-led-red-6', name: 'kitchen led red 6', isLight: true })
   const { humidityThing, temperatureThing, stop } = makeDHTThing(18)
   const server = new WebThingServer(new MultipleThings([
     blackButtonThing, redButtonThing,
