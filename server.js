e76f98c616fca0a81711181c2abcc3c5e6cfe89a
cescobaz <francesco.burelli@gmail.com>
2020-11-21 00:24:54 +0000

    leds order fix

--- server.js
+++ server.js
@@ -11,12 +11,12 @@ function runServer () {
   const redButtonThing = makeButtonThing(25, true, 'red-button-0', 'kitchen red button')
   const kitchenLamp = makeLedThing(24, 'kitchen-lamp-0', 'kitchen lamp')
   const kitchenBuzzer = makeLedThing(8, 'kitchen-buzzer-0', 'kitchen buzzer')
-  const kitchenLed1 = makeLedThing(4, 'kitchen-led-green-1', 'kitchen led green 1')
-  const kitchenLed2 = makeLedThing(17, 'kitchen-led-green-2', 'kitchen led green 2')
-  const kitchenLed3 = makeLedThing(22, 'kitchen-led-yellow-3', 'kitchen led yellow 3')
-  const kitchenLed4 = makeLedThing(10, 'kitchen-led-yellow-4', 'kitchen led yellow 4')
-  const kitchenLed5 = makeLedThing(9, 'kitchen-led-red-5', 'kitchen led red 5')
-  const kitchenLed6 = makeLedThing(11, 'kitchen-led-red-6', 'kitchen led red 6')
+  const kitchenLed1 = makeLedThing(11, 'kitchen-led-green-1', 'kitchen led green 1')
+  const kitchenLed2 = makeLedThing(9, 'kitchen-led-green-2', 'kitchen led green 2')
+  const kitchenLed3 = makeLedThing(10, 'kitchen-led-yellow-3', 'kitchen led yellow 3')
+  const kitchenLed4 = makeLedThing(22, 'kitchen-led-yellow-4', 'kitchen led yellow 4')
+  const kitchenLed5 = makeLedThing(17, 'kitchen-led-red-5', 'kitchen led red 5')
+  const kitchenLed6 = makeLedThing(4, 'kitchen-led-red-6', 'kitchen led red 6')
   const { humidityThing, temperatureThing, stop } = makeDHTThing(18)
   const server = new WebThingServer(new MultipleThings([
     blackButtonThing, redButtonThing,
