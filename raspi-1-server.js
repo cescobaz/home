642ba4184a083481c2161e2799b282dd49d746e5
Francesco Burelli <francesco.burelli@gmail.com>
2020-11-22 18:05:57 +0100

    takeSnapshot implemented

--- raspi-1-server.js
+++ raspi-1-server.js
@@ -6,17 +6,41 @@ const {
 } = require('webthing')
 const { makeLedThing } = require('./led-thing')
 const { makeVideoCameraHLS } = require('./video-camera-hls')
+const { exec } = require('child_process')
+
+const mediaDirectory = '/tmp/hls'
+const imageFilename = 'snapshot.jpg'
+
+function takeSnapshot (destinationPath) {
+  return new Promise((resolve, reject) => {
+    exec(`raspistill -o "${destinationPath}"`, (error) => {
+      if (error) {
+        reject(error)
+        return
+      }
+      resolve(destinationPath)
+    })
+  })
+}
 
 function runServer () {
-  const livingLamp = makeLedThing({ pin: 17, identifier: 'living-lamp-0', name: 'living lamp', isLight: true, inverted: true })
-  const picamera = makeVideoCameraHLS({ identifier: 'living-camera-0', name: 'living camera', hlsHref: '/playlist.m3u8', imageHref: '/snapshot.jpg' })
+  const livingLamp = makeLedThing({ pin: 16, identifier: 'living-lamp-0', name: 'living lamp', isLight: true, inverted: true })
+  const picamera = makeVideoCameraHLS({
+    identifier: 'living-camera-0',
+    name: 'living camera',
+    hlsFilename: 'playlist.m3u8',
+    imageFilename,
+    takeSnapshot: () => takeSnapshot(`${mediaDirectory}/${imageFilename}`)
+  })
   const routes = [
-	  { path: '/snapshot.jpg', handler: (req, res, next) => {
-	  res.download('/tmp/hls/snapshot.jpg', function (err) {
-		      if (err) return next(err);
-	  })
-	  
-	  } }
+    {
+      path: '/snapshot.jpg',
+      handler: (req, res, next) => {
+        res.download('/tmp/hls/snapshot.jpg', function (err) {
+          if (err) return next(err)
+        })
+      }
+    }
   ]
   const server = new WebThingServer(new MultipleThings([livingLamp, picamera], 'raspi-1'), 8888, null, null, routes)
   process.on('SIGINT', () => {
--- video-camera-hls.js
+++ video-camera-hls.js
@@ -6,30 +6,46 @@ const {
   Value
 } = require('webthing')
 
-function makeVideoCameraHLS ({ identifier, name, hlsHref, imageHref }) {
+function makeVideoCameraHLS ({ identifier, name, hlsFilename, imageFilename, mediaDirectory, takeSnapshot }) {
   const thing = new Thing(
     `urn:dev:ops:${identifier}`,
     name,
     ['VideoCamera', 'Camera'],
     name)
-  const videoValue = new Value(null, (v) => console.log('video', v))
+  const videoValue = new Value(null)
   thing.addProperty(
     new Property(thing, 'video', videoValue, {
       '@type': 'VideoProperty',
       title: 'Streaming',
-      links: [{ rel: 'alternate', mediaType: 'application/vnd.apple.mpegurl', hlsHref }],
+      links: [{ rel: 'alternate', mediaType: 'application/vnd.apple.mpegurl', hlsHref: `${mediaDirectory}/${hlsFilename}` }],
       readOnly: true
     }))
-  const imageLink = { rel: 'alternate', mediaType: 'image/jpeg', href: imageHref }
-  const imageValue = new Value(null, (v) => console.log('snapshot', v))
+  const imageValue = new Value(null)
   thing.addProperty(
     new Property(thing, 'image', imageValue, {
       '@type': 'ImageProperty',
       title: 'Snapshot',
-      links: [imageLink],
+      links: [{ rel: 'alternate', mediaType: 'image/jpeg', href: `${mediaDirectory}/${imageFilename}` }],
       readOnly: true
     }))
-  return thing
+  const mediaRoute = {
+    path: '/media/:file(*)',
+    handler: (req, res, next) => {
+      const requestedFile = req.params.file
+      if (requestedFile === imageFilename) {
+        takeSnapshot().then(() => {
+          res.download(`${mediaDirectory}/${requestedFile}`, function (err) {
+            if (err) return next(err)
+          })
+        }).catch(console.log)
+        return
+      }
+      res.download(`${mediaDirectory}/${requestedFile}`, function (err) {
+        if (err) return next(err)
+      })
+    }
+  }
+  return { thing, mediaRoute }
 }
 
 module.exports = { makeVideoCameraHLS }
