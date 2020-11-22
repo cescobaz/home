08cbc33db6aff1e5df2f78aa07e683857803feee
cescobaz <francesco.burelli@gmail.com>
2020-11-22 16:39:11 +0000

    experimental snapshot working

--- raspi-1-server.js
+++ raspi-1-server.js
@@ -9,8 +9,16 @@ const { makeVideoCameraHLS } = require('./video-camera-hls')
 
 function runServer () {
   const livingLamp = makeLedThing({ pin: 17, identifier: 'living-lamp-0', name: 'living lamp', isLight: true, inverted: true })
-  const picamera = makeVideoCameraHLS({ identifier: 'living-camera-0', name: 'living camera', hlsHref: 'http://raspi-1:8085/playlist.m3u8' })
-  const server = new WebThingServer(new MultipleThings([livingLamp, picamera], 'raspi-1'), 8888)
+  const picamera = makeVideoCameraHLS({ identifier: 'living-camera-0', name: 'living camera', hlsHref: '/playlist.m3u8', imageHref: '/snapshot.jpg' })
+  const routes = [
+	  { path: '/snapshot.jpg', handler: (req, res, next) => {
+	  res.download('/tmp/hls/snapshot.jpg', function (err) {
+		      if (err) return next(err);
+	  })
+	  
+	  } }
+  ]
+  const server = new WebThingServer(new MultipleThings([livingLamp, picamera], 'raspi-1'), 8888, null, null, routes)
   process.on('SIGINT', () => {
     Promise.race([
       server.stop(),
--- video-camera-hls.js
+++ video-camera-hls.js
@@ -6,24 +6,27 @@ const {
   Value
 } = require('webthing')
 
-function makeVideoCameraHLS ({ identifier, name, hlsHref }) {
+function makeVideoCameraHLS ({ identifier, name, hlsHref, imageHref }) {
   const thing = new Thing(
     `urn:dev:ops:${identifier}`,
     name,
     ['VideoCamera', 'Camera'],
     name)
+  const videoValue = new Value(null, (v) => console.log('video', v))
   thing.addProperty(
-    new Property(thing, 'pushed', null, {
+    new Property(thing, 'video', videoValue, {
       '@type': 'VideoProperty',
       title: 'Streaming',
-      links: [{ rel: 'alternate', mediaType: 'application/vnd.apple.mpegurl', hlsHref: '' }],
-      description: 'Whether the button is pressed',
+      links: [{ rel: 'alternate', mediaType: 'application/vnd.apple.mpegurl', hlsHref }],
       readOnly: true
     }))
+  const imageLink = { rel: 'alternate', mediaType: 'image/jpeg', href: imageHref }
+  const imageValue = new Value(null, (v) => console.log('snapshot', v))
   thing.addProperty(
-    new Property(thing, 'image', null, {
+    new Property(thing, 'image', imageValue, {
       '@type': 'ImageProperty',
       title: 'Snapshot',
+      links: [imageLink],
       readOnly: true
     }))
   return thing
