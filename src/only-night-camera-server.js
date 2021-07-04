c96ab3acf94e9d2a4329cb73f748d3ca0dcfe9fa
Francesco Burelli <francesco.burelli@gmail.com>
2021-07-04 19:42:28 +0200

    raspi-3-server changed

---
+++ src/only-night-camera-server.js
@@ -0,0 +1,39 @@
+'use strict'
+
+const {
+  MultipleThings,
+  WebThingServer
+} = require('webthing')
+const { makeLedThing } = require('./lib/led-thing')
+const { makeVideoCameraHLS, takeSnapshotRaspi } = require('./lib/video-camera')
+const fs = require('fs')
+
+const mediaDirectory = '/tmp/webthing-camera-media'
+const imageFilename = 'snapshot.jpg'
+
+function runServer (locationName, hostname) {
+  fs.mkdir(mediaDirectory, { recursive: true }, console.log)
+  const { videoCamera, mediaRoute } = makeVideoCameraHLS({
+    identifier: 'bedroom-camera-0',
+    name: 'bedroom camera',
+    dashFilename: 'playlist.mpd',
+    imageFilename,
+    mediaDirectory,
+    takeSnapshot: () => {
+      const now = new Date()
+      const destinationPath = `${mediaDirectory}/snapshot-${now}.jpg`
+      return takeSnapshotRaspi(destinationPath, ['--exposure', 'night', '--awb', 'greyworld'])
+    },
+    videoArgs: ['--awb', 'greyworld']
+  })
+  const server = new WebThingServer(new MultipleThings([videoCamera], 'raspi-3'), 8888, null, null, [mediaRoute])
+  process.on('SIGINT', () => {
+    Promise.race([
+      server.stop(),
+      new Promise((resolve) => setTimeout(resolve, 2000))
+    ]).finally(() => process.exit())
+  })
+  server.start().catch(console.error)
+}
+
+module.exports = { runServer }
--- src/raspi-3-server.js
+++ src/raspi-3-server.js
@@ -1,38 +1,9 @@
 'use strict'
 
-const {
-  MultipleThings,
-  WebThingServer
-} = require('webthing')
-const { makeVideoCameraHLS, takeSnapshotRaspi } = require('./lib/video-camera')
-const fs = require('fs')
-
-const mediaDirectory = '/tmp/webthing-camera-media'
-const imageFilename = 'snapshot.jpg'
+const server = require('./src/only-camera-server')
 
 function runServer () {
-  fs.mkdir(mediaDirectory, { recursive: true }, console.log)
-  const { videoCamera, mediaRoute } = makeVideoCameraHLS({
-    identifier: 'bedroom-camera-0',
-    name: 'bedroom camera',
-    dashFilename: 'playlist.mpd',
-    imageFilename,
-    mediaDirectory,
-    takeSnapshot: () => {
-      const now = new Date()
-      const destinationPath = `${mediaDirectory}/snapshot-${now}.jpg`
-      return takeSnapshotRaspi(destinationPath, ['--exposure', 'night', '--awb', 'greyworld'])
-    },
-    videoArgs: ['--awb', 'greyworld']
-  })
-  const server = new WebThingServer(new MultipleThings([videoCamera], 'raspi-3'), 8888, null, null, [mediaRoute])
-  process.on('SIGINT', () => {
-    Promise.race([
-      server.stop(),
-      new Promise((resolve) => setTimeout(resolve, 2000))
-    ]).finally(() => process.exit())
-  })
-  server.start().catch(console.error)
+  server.runServer('bedroom-ceil', 'raspi-3')
 }
 
 module.exports = { runServer }
